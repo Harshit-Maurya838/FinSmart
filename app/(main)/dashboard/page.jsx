5b531a83471cc96499a9ef86e3ea50b6076c72ab
Harshit-Maurya838 <44harshitmaurya@gmail.com>
2025-05-11 10:07:29 +0530

    completed sending mail for budget-alert

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -4,12 +4,26 @@ import { Card, CardContent } from '@/components/ui/card'
 import { Plus } from 'lucide-react'
 import React from 'react'
 import AccountCard from './_components/account-card'
+import { getCuurentBudget } from '@/actions/budget'
+import { BudgetProgress } from './_components/budget-progress'
 
 const DashboardPage = async() => {
   const accounts = await getUserAccounts();
+
+  const defaultAccount = accounts?.find((account) => account.isDefault);
+
+  let budgetData = null;
+  if(defaultAccount){
+    budgetData = await getCuurentBudget(defaultAccount.id)
+  }
+
   return (
-    <div className='px-5'>
+    <div className='space-y-8'>
       {/* Budget Overview */}
+        <BudgetProgress
+        initialBudget={budgetData?.budget}
+        currentExpenses={budgetData?.currentExpenses || 0}
+      />
 
       {/* Overview  */}
 
