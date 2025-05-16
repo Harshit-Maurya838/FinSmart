fdb27001fd8ca5736ba54b77aa879550c4820b6f
Harshit-Maurya838 <44harshitmaurya@gmail.com>
2025-05-16 17:54:16 +0530

    explicitly added / and /robots.txt

--- middleware.js
+++ middleware.js
@@ -47,6 +47,8 @@ export default createMiddleware(aj, clerk);
 
 export const config = {
   matcher: [
+    "/",
+    "/robots.txt",
     // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
